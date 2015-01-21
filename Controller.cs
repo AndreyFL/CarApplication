e31f95ffe7bf89cd25d8e7db5808bcf3778808b7
AndreyFL <AndreyFilchenkov@gmail.com>
2015-01-21 23:42:42 +0200

    First Issue

---
+++ Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CarLibrary;
+
+namespace CarApplication
+{
+    class Controller
+    {
+        Car myNewCar;
+
+        public Controller()
+        {
+            myNewCar = new Car("Flash", 160, 15);
+        }
+
+        public void SowCarState()
+        {
+            Console.WriteLine("Car \"{0}\", CurrSpeed {1}, Acceleration {2}, Engine is {3}.",
+                myNewCar.CarName, myNewCar.CurrSpeed, myNewCar.Acceleration, myNewCar.IsEngineOn?"on":"off");
+        }
+
+        public void StartStopEngine()
+        {
+            myNewCar.IsEngineOn = !myNewCar.IsEngineOn;
+        }
+
+        public void Fastly()
+        {
+            myNewCar.SpeedUp();
+        }
+        public void Slowly()
+        {
+            myNewCar.SpeedDown();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CarLibrary;
+
+namespace CarApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller carCtrlr = new Controller();
+
+
+            ConsoleKeyInfo key;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Завести/заглушить двигатель автомобиля.");
+                Console.WriteLine("2. Увеличить скорость скорость.");
+                Console.WriteLine("3. Уменьшить скорость.");
+                Console.WriteLine("\nq. Выход.\n");
+                carCtrlr.SowCarState();
+                key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        carCtrlr.StartStopEngine();
+                        break;
+                    case ConsoleKey.D2:
+                        carCtrlr.Fastly();
+                        break;
+                    case ConsoleKey.D3:
+                        carCtrlr.Slowly();
+                        break;
+                }
+
+
+                
+            } while (key.KeyChar.ToString().ToLower() != "q");
+
+        }
+    }
+}
